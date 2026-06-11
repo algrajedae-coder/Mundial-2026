37ba01d3177fe6e1bd82f7a3ac358fabd94bc682
algrajedae-coder <algrajedae@gmail.com>
2026-06-11 15:21:06 -0600

    Update firebase.ts

--- project/src/app/lib/firebase.ts
+++ project/src/app/lib/firebase.ts
@@ -1,18 +1,18 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getMessaging, isSupported } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDummyKeyForNow",
-  authDomain: "quiniela-2026.firebaseapp.com",
-  projectId: "quiniela-2026",
-  storageBucket: "quiniela-2026.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:dummy"
+  apiKey: "AIzaSyCNKBf_-dcgiqMYHxfRqKDo4mX-Q6PcWDo",
+  authDomain: "quiniela2026-51e1e.firebaseapp.com",
+  projectId: "quiniela2026-51e1e",
+  storageBucket: "quiniela2026-51e1e.firebasestorage.app",
+  messagingSenderId: "99987296667",
+  appId: "1:99987296667:web:8c52d6b41c1546236c513e"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
 const db = getFirestore(app);
 const googleProvider = new GoogleAuthProvider();
